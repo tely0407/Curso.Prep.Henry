a3ad6e14625be33fe842bb375f7a8ba25e20f874
tely0407 <equadrio2006@hotmail.com>
2021-12-01 14:45:13 -0300

    VIhomeworktelyquadrio

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -6,7 +6,7 @@ function crearUsuario() {
   // En el `contructor`, define el usuario, el nombre, el email y la contraseña
   // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
-  // Devuelve la clase
+  // Devuelve la clase...
   // Tu código:
   function Usuario(opciones) {
   this.usuario=opciones.usuario;
