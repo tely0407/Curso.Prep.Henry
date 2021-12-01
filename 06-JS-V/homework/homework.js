adb2484930b818b5fd86fbfe0e25bacd8851e90d
tely0407 <equadrio2006@hotmail.com>
2021-12-01 14:51:12 -0300

    06-JS-V_homework

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,7 +8,7 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase...
   // Tu código:
-  function Usuario(opciones) {
+  function Usuario(opciones){
   this.usuario=opciones.usuario;
   this.nombre= opciones.nombre;
   this.email= opciones.email;
